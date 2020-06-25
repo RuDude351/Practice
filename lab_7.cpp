6335fdc5083863333d62777c763389df8e86b0d7
RuDude351 <rudude351@gmail.com>
2020-06-25 18:21:12 +0300

    7_1

---
+++ lab_7.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <locale.h>
+#define N 5
+//ввод элементов
+void inpm(int a[N][N]) {
+	int i, j;
+	printf("Укажите элементы \n");
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++) {
+			printf("a[%d][%d]:", i, j);
+			scanf_s("%d", &a[i][j]);
+		}
+	}
+}
