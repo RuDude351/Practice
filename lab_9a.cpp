628c02d44d3a74b5d95c68cf3a32b9c3d28abd6f
RuDude351 <rudude351@gmail.com>
2020-07-02 15:48:47 +0300

    9a_2

--- lab_9a.cpp
+++ lab_9a.cpp
@@ -4,6 +4,7 @@
 #include <malloc.h>
 #include <stdlib.h>
 #include <locale.h>
+//ввод
 void inp(int** a, int n)
 {
 	int i, j;
@@ -16,15 +17,54 @@ void inp(int** a, int n)
 		}
 	}
 }
+//вывод
+void outp(int** a, int n)
+{
+	int i, j;
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			printf("%5d ", a[i][j]);
+		}
+		printf("\n");
+	}
+}
 int main()
 {
 	setlocale(LC_ALL, "");
 	int** a;
-	int i, n;
+	int i, n, j, k, max;
 	printf("Размерность: ");
 	scanf("%d", &n);
 	a = new int* [n];
 	for (i = 0; i < n; i++)
 		a[i] = new int[n];
 	inp(a, n);
+	printf("Массив:\n");
+	outp(a, n);
+	k = 0;
+	for (i = 0; i < n; i++)
+	{
+		max = a[i][0];
+		for (j = 0; j < n; j++)
+		{
+			if (a[i][j] > max)
+			{
+				max = a[i][j];
+				k = j;
+			}
+		}
+		if (k != 0)
+		{
+			for (j = 0; j < k; j++)
+				a[i][j] = 0;
+		}
+	}
+	printf("Результат:\n");
+	outp(a, n);
+	for (i = 0; i < n; i++)
+		free(a[i]);
+	free(a);
+	getchar();
 }
